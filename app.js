fd35aa16f5a8c5d17cb06c7d235013d1daf69105
JudeChukwuma14 <ebukajude14@gmail.com>
2025-03-31 15:47:11 +0100

    cor

--- app.js
+++ app.js
@@ -19,11 +19,7 @@ app.use(cookie())
 app.use(express.urlencoded({ extended:true}))
 app.use(express.json());
 
-app.use(cors({
-    origin:"https://jannode.onrender.com",
-    credentials:true,
-    methods:["GET", "POST", "PUT", "DELETE"],
-    }))
+app.use(cors)
 
 app.use("/api/vp1", allRoutes)
 const port = process.env.PORT || 4000;
